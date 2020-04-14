ab84554ce715841609b8eea1d7163fd124ffacdd
Clara Lan <clarajylan@gmail.com>
2020-04-14 08:55:30 -0400

    feat: add react routers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
+import Header from './Header';
 
+
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing!</h2>;
 const App = () => {
   return (
     <div>
-      Hello World!
+      <BrowserRouter>
+      {/*has only one child*/}
+        <div>
+          {/*header here will like text between routes, which will ne always visible*/}
+          <Header />
+          <Route exact path='/' component = {Landing} />
+          <Route exact path='/surveys' component = {Dashboard} />
+          <Route path='/surveys/new' component={SurveyNew} /> 
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
