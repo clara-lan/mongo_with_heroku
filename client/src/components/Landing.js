cb3686ebb39f8fb9fe6077d8d84c9417d5f4b234
Clara Lan <clarajylan@gmail.com>
2020-05-20 22:08:52 -0400

    chores

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return(
@@ -6,7 +7,9 @@ const Landing = () => {
       <h1>
         Emaily!
       </h1>
+     <Link to="/surveys">
       Collect feedback!
+      </Link>
     </div>
   );
 };
